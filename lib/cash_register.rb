7eb3da90d4f5700f2df13cde452190c2dce54b7c
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-23 14:59:36 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,38 @@
+class CashRegister
+  attr_accessor :total, :discount, :price, :items, :last_transaction
 
+  def initialize(discount = 0)
+    @total= 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(item, price, quantity = 1)
+    @price = price
+    @total += price * quantity
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << item
+        counter += 1
+      end
+    else
+      @items << item
+    end
+    @last_transaction = price * quantity
+  end
+
+  def apply_discount
+    if @discount == 0
+      "There is no discount to apply."
+    else
+      @total -= @total * @discount / 100
+      "After the discount, the total comes to $#{self.total}."
+    end
+  end
+
+  def void_last_transaction
+    @total -= @last_transaction
+  end
+
+end 
