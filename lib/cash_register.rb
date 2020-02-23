bf3cd50f384c89f6ef4e46731a0062373d57c927
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-23 14:54:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
 
+class CashRegister
+  
+  attr_accessor :total, :discount
+  
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def total 
+    self.total
+  end
+  
+end
