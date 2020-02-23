137bb2fa2b9fb7ca940699d968da33760d8fee82
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-23 14:55:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,15 +1,34 @@
 
 class CashRegister
   
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :items, :last_price
   
   def initialize(discount=0)
     @total = 0
     @discount = discount
+    @items = []
+    @last_price = 0
   end
   
-  def total 
-    self.total
+  def add_item(title, price, inc_total, quantity)
+    self.items += [item] * quantity
+    self.last_price = price * quantity
+    self.total += price * quantity
   end
+  
+  def apply_discount
+    if self.discount > 0
+      self.total *= (1 - self.discount / 100.0)
+      "After the discount, the total comes to $#{self.total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    self.items.pop
+    self.total -= self.last_price
+  end
+
   
 end
